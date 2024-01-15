f03dc10e9e3df47c8d77b0f5a5ecef6a704e1319
Adam Essenmacher <adam@scavos.com>
2024-01-15 03:44:42 -0500

    Initial commit

---
+++ src/GCCollectionMonitor.Maui/Demo.MainPage/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+
+namespace Demo.MainPage;
+
+public partial class App
+{
+    private readonly GCCollectionMonitor.Maui.GCCollectionMonitor _monitor;
+    private Page? _currentPage;
+
+    public App()
+    {
+        InitializeComponent();
+
+        _monitor = new GCCollectionMonitor.Maui.GCCollectionMonitor();
+
+        PropertyChanged += HandleMainPageChanged;
+
+        MainPage = new NavigationPage(new MainPage());
+    }
+
+    private void HandleMainPageChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName != nameof(MainPage))
+            return;
+
+        Page? lastPage = _currentPage;
+        if (lastPage is NavigationPage lastNavPage)
+            lastNavPage.Popped -= NavPageOnPopped;
+        
+        _currentPage = MainPage;
+        if(_currentPage is NavigationPage currentNavPage)
+            currentNavPage.Popped += NavPageOnPopped;
+
+        if (lastPage != null)
+            _monitor.MonitorForCollection(lastPage);
+
+        _monitor.CollectAndCheck();
+    }
+
+    private void NavPageOnPopped(object? sender, NavigationEventArgs e)
+    {
+        _monitor.MonitorForCollection(e.Page);
+        _monitor.CollectAndCheck();
+    }
+}
---
+++ src/GCCollectionMonitor.Maui/GCCollectionMonitor.Maui/GCCollectionMonitor.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace GCCollectionMonitor.Maui;
+
+public class GCCollectionMonitor
+{
+    private readonly List<Tuple<string, WeakReference<object>>> _references = [];
+
+    public void MonitorForCollection(object target)
+    {
+        string targetType = target.GetType().Name;
+        _references.Add(new Tuple<string, WeakReference<object>>(targetType, new WeakReference<object>(target)));
+    }
+
+    public async void CollectAndCheck()
+    {
+        const int maxCollections = 5;
+        var currentCollection = 0;
+        while (++currentCollection <= maxCollections && _references.Count != 0)
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            foreach (Tuple<string, WeakReference<object>> reference in _references.ToArray())
+                if (reference.Item2.TryGetTarget(out object? target))
+                {
+                    if (currentCollection == maxCollections)
+                    {
+                        Debug.WriteLine($"🧟 {target.GetType().Name} is a zombie!");
+                        _references.Remove(reference);
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine($"✅{reference.Item1} released after {currentCollection} collections");
+                    _references.Remove(reference);
+                }
+
+            await Task.Delay(500);
+        }
+    }
+}
